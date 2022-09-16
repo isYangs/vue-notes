86f56d49d47f2c8197dd6c254abbfd45f997acef
isYangs <xuewuzhibu@outlook.com>
2022-09-16 11:15:21 +0800

    📝 完成Vue组件化编程

---
+++ Vue组件化编程/单文件组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <User />
+        <Blog />
+    </div>
+</template>
+
+<script>
+// 引入组件
+import User from './User';
+import Blog from './Blog';
+
+export default {
+    name: 'App',
+    components: {
+        User,
+        Blog,
+    },
+};
+</script>
---
+++ Vue组件化编程/单文件组件/main.js
@@ -0,0 +1,7 @@
+import App from './App'
+
+new Vue({
+    el: '#app',
+    components: { App },
+    template: '<App/>'
+})
