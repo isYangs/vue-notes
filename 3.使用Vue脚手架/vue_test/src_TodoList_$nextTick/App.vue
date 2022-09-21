f2d53504981a1f6e01cac2f71220f25a317ce3b0
isYangs <xuewuzhibu@outlook.com>
2022-09-21 15:26:28 +0800

    📝 更新至使用nextTick优化TodoList案例

---
+++ 3.使用Vue脚手架/vue_test/src_TodoList_$nextTick/App.vue
@@ -0,0 +1,133 @@
+<template>
+    <div id="root">
+        <div class="todo-container">
+            <div class="todo-wrap">
+                <AddTodo @addTodo="addTodo" />
+                <TodoList :todoData="todoData" />
+                <RemoveTodo
+                    :todoData="todoData"
+                    @checkAllTodo="checkAllTodo"
+                    @clearAllTodo="clearAllTodo"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import pubsub from 'pubsub-js'; // 第三方库写在最上面
+import AddTodo from './components/AddTodo.vue';
+import TodoList from './components/TodoList.vue';
+import RemoveTodo from './components/RemoveTodo.vue';
+
+export default {
+    name: 'App',
+    components: {
+        AddTodo,
+        TodoList,
+        RemoveTodo,
+    },
+    data() {
+        return {
+            todoData: JSON.parse(localStorage.getItem('todoData')) || [],
+        };
+    },
+    methods: {
+        // 添加todo
+        addTodo(todoObj) {
+            this.todoData.unshift(todoObj);
+        },
+        // 勾选 or 取消勾选一个todo
+        checkTodo(id) {
+            this.todoData.forEach(todo => {
+                if (todo.id === id) todo.done = !todo.done;
+            });
+        },
+        updateTodo(id,title) {
+            this.todoData.forEach(todo => {
+                if (todo.id === id) todo.title = title;
+            });
+        },
+        // 删除一个todo
+        deleteTodoItem(_, id) {
+            this.todoData = this.todoData.filter(todo => todo.id !== id);
+        },
+        // 是否全选
+        checkAllTodo(done) {
+            this.todoData.forEach(todo => {
+                todo.done = done;
+            });
+        },
+        // 清楚所有已完成的todo
+        clearAllTodo() {
+            this.todoData = this.todoData.filter(todo => !todo.done);
+        },
+    },
+    watch: {
+        todoData: {
+            deep: true,
+            handler(val) {
+                localStorage.setItem('todoData', JSON.stringify(val));
+            },
+        },
+    },
+    mounted() {
+        this.$bus.$on('checkTodo', this.checkTodo);
+        this.$bus.$on('updateTodo', this.updateTodo);
+        this.pubId = pubsub.subscribe('deleteTodoItem', this.deleteTodoItem);
+    },
+    beforeDestroy() {
+        this.$bus.$off('checkTodo');
+        this.$bus.$off('updateTodo');
+        pubsub.unsubscribe(this.pubId);
+    },
+};
+</script>
+
+<style>
+body {
+    background: #fff;
+}
+.btn {
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+}
+.btn-danger {
+    color: #fff;
+    background-color: #da4f49;
+    border: 1px solid #bd362f;
+}
+.btn-danger:hover {
+    color: #fff;
+    background-color: #bd362f;
+}
+.btn-edit {
+    color: #fff;
+    background-color: #5bc0de;
+    border: 1px solid #46b8da;
+}
+.btn-edit:hover {
+    color: #fff;
+    background-color: #46b8da;
+}
+.btn:focus {
+    outline: none;
+}
+.todo-container {
+    width: 600px;
+    margin: 0 auto;
+}
+.todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+}
+</style>
