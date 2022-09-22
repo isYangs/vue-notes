3ea8715f429614b520b26cab41b3912a43d925ba
isYangs <xuewuzhibu@outlook.com>
2022-09-22 10:05:26 +0800

    📝 更新至Vue脚手架配置代理，修复Bug

--- db/README.md
+++ db/README.md
@@ -11,8 +11,14 @@ npm install -g json-server
 
 ```
 cd db
-json-server -w student.json
+json-server -w students.json
 ```
+
+见下图所示
+
+![1663809797585.png](https://a.xuewuzhibu.cn/1/632bb91135c74-1.png)
+
+![1663809906656.png](https://a.xuewuzhibu.cn/1/632bb977dc3d6-1.png)
 
 > 注意：请在两个终端中分别执行，如果你的端口被占用，可以使用`-p`参数指定端口，但是要确保两个端口不能相同
 
--- vue_test/vue.config.js
+++ vue_test/vue.config.js
@@ -1,5 +1,27 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
-})
+    transpileDependencies: true,
+    // 关闭语法检查
+    lintOnSave: false,
+    // 配置代理服务器（方式一）
+    /* devServer: {
+    proxy: 'http://localhost:3000'
+  } */
+    // 配置代理服务器（方式二）
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                pathRewrite: { '^/api': '' },
+                // ws: true, // 用于支持websocket
+                // changeOrigin: true, // 用于控制请求头中的host值
+            },
+            '/demo': {
+                target: 'http://localhost:3001',
+                pathRewrite: { '^/demo': '' },
+                // ws: true, // 用于支持websocket
+                // changeOrigin: true, // 用于控制请求头中的host值
+            },
+        },
+    },
+});
