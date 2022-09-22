43a37eff30da4624d9b8882142b33cf099577d74
isYangs <xuewuzhibu@outlook.com>
2022-09-22 15:26:02 +0800

    📝 更新至默认插槽

---
+++ vue_test/src/components/Category.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="category">
+        <h3>{{ title }}分类</h3>
+        <!-- 定义一个插槽 -->
+        <!-- <slot>我是默认值，当组件的使用者没有传递时，我就会出现</slot> -->
+        <slot>
+            <ul>
+                <li v-for="(item, index) in listData" :key="index">{{ item }}</li>
+            </ul>
+        </slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Category',
+    props: ['title', 'listData'],
+};
+</script>
+
+<style scoped>
+.category {
+    width: 200px;
+    height: 300px;
+    background: skyblue;
+}
+h3 {
+    text-align: center;
+    background: orange;
+}
+
+img,
+video {
+    width: 100%;
+}
+</style>
