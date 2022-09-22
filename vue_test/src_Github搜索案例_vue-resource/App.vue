dce18bbcf10d7ab27e48915786017ab6341fea62
isYangs <xuewuzhibu@outlook.com>
2022-09-22 14:38:13 +0800

    📝 更新至vue-resource

---
+++ vue_test/src_Github搜索案例_vue-resource/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="root">
+        <Search />
+        <List />
+    </div>
+</template>
+
+<script>
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import Search from './components/Search.vue';
+import List from './components/List.vue';
+export default {
+    name: 'App',
+    components: {
+        Search,
+        List,
+    },
+};
+</script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+}
+a {
+    text-decoration: none;
+    color: #000;
+}
+.root {
+    width: 80%;
+    margin: 0 auto;
+}
+</style>
