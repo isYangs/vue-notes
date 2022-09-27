a5a92f481b5fe5484936b2b39bbdc21ae7fb3717
isYangs <xuewuzhibu@outlook.com>
2022-09-27 14:59:57 +0800

    :memo: 更新至路由的props配置

---
+++ vue_test/src_路由的props配置/pages/About.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>我是About组件</h2>
+</template>
+
+<script>
+export default {
+    name: 'About',
+
+    /*beforeDestroy() {
+        console.log('About组件即将被销毁');
+    },*/
+};
+</script>
