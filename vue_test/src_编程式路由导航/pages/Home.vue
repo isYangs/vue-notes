4f91da7cb9f8b105f98b8d3d183701c5f620002e
isYangs <xuewuzhibu@outlook.com>
2022-09-27 16:13:37 +0800

    :memo: 编程式路由导航

---
+++ vue_test/src_编程式路由导航/pages/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>我是Home组件</h2>
+        <ul class="nav nav-tabs">
+            <li>
+                <router-link class="list-group-item" active-class="active" to="/home/news">
+                    News
+                </router-link>
+            </li>
+            <li>
+                <router-link class="list-group-item" active-class="active" to="/home/message">
+                    Message
+                </router-link>
+            </li>
+        </ul>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    /*mounted() {
+        console.log('Home组件即将被挂载', this);
+    },*/
+    /*beforeDestroy() {
+        console.log('Home组件即将被销毁');
+    },*/
+};
+</script>
--- vue_test/src_路由跳转的replace方式/router/index.js
+++ vue_test/src_路由跳转的replace方式/router/index.js
@@ -26,7 +26,7 @@ export default new VueRouter({
                     children: [
                         {
                             name: 'detail',
-                            path: 'detail/:id/:title',
+                            path: 'detail',
                             component: Detail,
                             // props的第一种写法，值为对象，该对象中的所有key-value都会以props的形式传给Detail组件。
                             // props: { a: 1, b: 'hello' },  // 不常用
