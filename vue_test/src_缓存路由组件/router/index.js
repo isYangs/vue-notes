e49127db7c5f49c3149a38a00da45aa5befa4e89
isYangs <xuewuzhibu@outlook.com>
2022-09-28 09:29:57 +0800

    :memo: 缓存路由组件

---
+++ vue_test/src_缓存路由组件/router/index.js
@@ -0,0 +1,40 @@
+// 该文件专门用于创建整个应用的路由器
+import VueRouter from 'vue-router';
+import About from '@/pages/About';
+import Home from '@/pages/Home';
+import Message from '@/pages/Message';
+import News from '@/pages/News';
+import Detail from '@/pages/Detail';
+
+export default new VueRouter({
+    routes: [
+        {
+            path: '/about',
+            component: About,
+        },
+        {
+            path: '/home',
+            component: Home,
+            children: [
+                {
+                    path: 'news',
+                    component: News,
+                },
+                {
+                    path: 'message',
+                    component: Message,
+                    children: [
+                        {
+                            name: 'detail',
+                            path: 'detail',
+                            component: Detail,
+                            props($route) {
+                                return { id: $route.query.id, title: $route.query.title };
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+});
