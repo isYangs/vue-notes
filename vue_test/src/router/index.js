cfb296a5796d67dc12366805a8ab36519bd5b742
isYangs <xuewuzhibu@outlook.com>
2022-09-28 10:42:35 +0800

    :memo: 全局后置-路由守卫

--- vue_test/src/router/index.js
+++ vue_test/src/router/index.js
@@ -12,26 +12,31 @@ const router = new VueRouter({
             name: 'about',
             path: '/about',
             component: About,
+            meta: { title: '关于' },
         },
         {
             name: 'home',
             path: '/home',
             component: Home,
+            meta: { title: '主页' },
             children: [
                 {
                     name: 'news',
                     path: 'news',
                     component: News,
+                    meta: { isAuth: true, title: '新闻' },
                 },
                 {
                     name: 'message',
                     path: 'message',
                     component: Message,
+                    meta: { isAuth: true, title: '消息' },
                     children: [
                         {
                             name: 'detail',
                             path: 'detail',
                             component: Detail,
+                            meta: { title: '详情' },
                             props($route) {
                                 return { id: $route.query.id, title: $route.query.title };
                             },
@@ -45,8 +50,9 @@ const router = new VueRouter({
 
 // 全局前置路由守卫-初始化的时候被调用、每次路由切换之前被调用
 router.beforeEach((to, from, next) => {
-    console.log(to, from);
-    if (to.name === 'news' || to.name === 'message') {
+    console.log('前置路由守卫', to, from);
+    // 判断是否需要鉴权
+    if (to.meta.isAuth) {
         if (localStorage.getItem('name') === 'isYangs') {
             next();
         } else {
@@ -55,6 +61,12 @@ router.beforeEach((to, from, next) => {
     } else {
         next();
     }
+});
+
+// 全局后置路由守卫-初始化的时候被调用、每次路由切换之后被调用
+router.afterEach((to, from) => {
+    console.log('后置路由守卫', to, from);
+    document.title = to.meta.title;
 });
 
 export default router;
