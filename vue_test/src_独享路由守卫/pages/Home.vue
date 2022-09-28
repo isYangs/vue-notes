4f2d6187f68c4d963f90d55c1aaacf7be58712f4
isYangs <xuewuzhibu@outlook.com>
2022-09-28 11:04:09 +0800

    :memo: 独享路由守卫

---
+++ vue_test/src_独享路由守卫/pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>我是Home组件</h2>
+        <ul class="nav nav-tabs">
+            <li>
+                <router-link class="list-group-item" active-class="active" to="/home/news">
+                    News
+                </router-link>
+            </li>
+            <li>
+                <router-link class="list-group-item" active-class="active" to="/home/message">
+                    Message
+                </router-link>
+            </li>
+        </ul>
+        <keep-alive include="News">
+            <router-view></router-view>
+        </keep-alive>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    /*mounted() {
+        console.log('Home组件即将被挂载', this);
+    },*/
+    /*beforeDestroy() {
+        console.log('Home组件即将被销毁');
+    },*/
+};
+</script>
